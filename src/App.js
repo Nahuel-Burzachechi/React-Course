9bfc1d9d6f5f31d6bad1847fbd7ab755b456683c
Nahuel Burzachechi <nburzachechi@andreani.com>
2022-02-25 20:35:31 -0300

    React Course

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Aprendiendo perri</h2>
+      <p>This is also visible!</p>
+      <ExpenseItem/>
     </div>
   );
 }
